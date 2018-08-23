f2f6e4d9bab016c2d443907c7e956d266b3b5962
Dawid Olejniczak <olejniczak.dawid@onet.pl>
2018-08-23 20:37:09 +0200

    Delete cities route

--- routes/api.php
+++ routes/api.php
@@ -7,7 +7,3 @@ $router->group(['prefix' => 'schools', 'as' => 'schools'], function () use ($rou
 $router->group(['prefix' => 'categories', 'as' => 'categories'], function () use ($router) {
     $router->get('/', ['as' => 'index', 'uses' => 'CategoriesController@index']);
 });
-
-$router->group(['prefix' => 'cities', 'as' => 'cities'], function () use ($router) {
-    $router->get('/', ['as' => 'index', 'uses' => 'CitiesController@index']);
-});
